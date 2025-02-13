10d11aaf088196d6e0b6f4e9c130aa243b15bec5
Nitin Sharma <nitinsharma9829547681@gmail.com>
2025-02-13 18:57:01 +0530

    Completed 3 assignments

---
+++ Lab Assignment - 02/Q2.java
@@ -0,0 +1,16 @@
+// Calculator to add two integers and two real numbers
+class Q2 {
+    static int add (int x, int y) {
+        return x + y;
+    }
+
+    static double add (double x, double y) {
+        return x + y;
+    }
+    // Making Static methods so that main can access them
+
+    public static void main(String args[]) {
+        System.out.println(add(5, 10)); // 15
+        System.out.println(add(5.5, 10.9)); // 16.4
+    }    
+}
